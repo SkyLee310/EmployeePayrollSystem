660e848ac66c024ec52e46ed3295243cc6bfa95e
SkyLee310 <leeskyxuan@gmail.com>
2025-12-16 15:05:11 +0800

    Create initial function

---
+++ main.py
@@ -0,0 +1,29 @@
+def load_data():
+    pass
+
+def working_hours():
+    pass
+
+def gross_pay():
+    pass
+
+def deductions():
+    pass
+
+def net_salary():
+    pass
+
+def generate_payslip():
+    pass
+
+def add_employee():
+    pass
+
+def remove_employee():
+    pass
+
+def update_employee():
+    pass
+
+def main():
+    pass
