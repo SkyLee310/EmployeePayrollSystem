ec46c7d8dc11b270683d43565766786384e6f53c
SkyLee310 <leeskyxuan@gmail.com>
2025-12-22 02:10:11 +0800

    Add new database:payslip_data and Create a function to write generate payslip history into it

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import pandas as pd
 import openpyxl
 import warnings
-
+from datetime import datetime
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def load_data(filename):
+def load_data(employee_file):
     df=None
     try:
-        df=pd.read_excel(filename)
+        df=pd.read_excel(employee_file)
     except FileNotFoundError:
         print("File does not found! Check whether filepath is valid.")
 
     if df is not None:
-        print("\nEmployee Database\n")
+        print("\n------------------Employee Database-------------------")
         print(df.head(),"\n")
 
 
     return df
 
-def working_hours(filename):
-    df=pd.read_excel(filename)
+def write_to_salary_file(salary_file,employee_id,normal_pay,ot_pay,total_gross_pay,epf,socso,after_deduct_pay):
+    df=pd.read_excel(salary_file)
+
+    new_row = pd.DataFrame({'employee_id': [employee_id],
+                            'datetime': [datetime.now()],
+                            'normal_hours_paid': [normal_pay],
+                            'ot_hours_paid': [ot_pay],
+                            'gross_pay': [total_gross_pay],
+                            'epf':[epf],
+                            'socso':[socso],
+                            'net_paid':[after_deduct_pay]})
+
+    df = pd.concat([df, new_row], ignore_index=True)
+    print(f"New Salary Record for Employee ID:{employee_id} has been added to the system!\n\n")
+
+    df.to_excel(salary_file, index=False)
+
+
+def working_hours(employee_file):
+    df=pd.read_excel(employee_file)
     id=input("Which employee working hours need to be calculate(ID): ")
 
     index_row_update = df[df['employee_id'].astype(str) == id]
@@ -46,8 +64,8 @@ def working_hours(filename):
 
     return normal_hours, ot_hours, id
 
-def gross_pay(filename,normal_hours,ot_hours,employee_id):
-    df=pd.read_excel(filename)
+def gross_pay(employee_file,normal_hours,ot_hours,employee_id):
+    df=pd.read_excel(employee_file)
 
     row_index = df[df['employee_id'].astype(str) == employee_id].index[0]
 
@@ -59,7 +77,7 @@ def gross_pay(filename,normal_hours,ot_hours,employee_id):
     print(f"OT Hours Paid: RM{ot_pay:.2f}")
     print(f"Total Gross Pay: RM{total_gross_pay:.2f}")
 
-    return total_gross_pay
+    return normal_pay,ot_pay,total_gross_pay
 
 def deductions(total_gross_pay):
     EPF = 0.11
@@ -67,33 +85,40 @@ def deductions(total_gross_pay):
 
     EPF_deduct=total_gross_pay*EPF
     SOCSO_deduct=total_gross_pay*SOCSO
-    print(f"Deduct EPF RM{EPF_deduct}")
-    print(f"Deduct SOCSO RM{SOCSO_deduct}")
+    print(f"Deduct EPF RM{EPF_deduct:.2f}")
+    print(f"Deduct SOCSO RM{SOCSO_deduct:.2f}")
     after_deduct_pay=total_gross_pay-EPF_deduct-SOCSO_deduct
-    return after_deduct_pay
+    return EPF_deduct,SOCSO_deduct,after_deduct_pay
 
 
 def net_salary(after_deduct_pay):
     print(f"Your Net Salary is RM{after_deduct_pay:.2f}\n\n")
 
-def generate_payslip(filename):
-    df=pd.read_excel(filename)
+def generate_payslip(employee_file,salary_file):
+    df=pd.read_excel(employee_file)
     print("---------Generate Payslip System-----------")
-    normal_hours, ot_hours, employee_id = working_hours(filename)
+    normal_hours, ot_hours, employee_id = working_hours(employee_file)
     row_index = df[df['employee_id'].astype(str) == employee_id].index[0]
     print(f"Employee's {df.loc[row_index,'name']} Payslip")
-    total_gross_pay = gross_pay(filename,normal_hours,ot_hours,employee_id)
-    after_deduct_pay= deductions(total_gross_pay)
+    normal_pay,ot_pay,total_gross_pay = gross_pay(employee_file,normal_hours,ot_hours,employee_id)
+    epf,socso,after_deduct_pay= deductions(total_gross_pay)
     print("After Deduct EPF and SOCSO:")
     net_salary(after_deduct_pay)
+    write_to_salary_file(salary_file, employee_id, normal_pay, ot_pay, total_gross_pay, epf, socso, after_deduct_pay)
 
 
-def add_employee(filename):
+def add_employee(employee_file):
 
-    df=pd.read_excel(filename)
+    df=pd.read_excel(employee_file)
 
     print("Add New Employee")
     id=input("Employee ID: ")
+    index_to_add = df[df['employee_id'].astype(str) == id].index
+
+    if len(index_to_add) != 0:
+        print(f"Employee {id} was already there!\n")
+        return
+
     name=input("Employee Name: ")
     rate=float(input("Hourly Rate: "))
     std_hour=float(input("Standard Working Hours: "))
@@ -108,12 +133,12 @@ def add_employee(filename):
     df=pd.concat([df,new_row], ignore_index=True)
     print(f"{name} has been added to the system!\n\n")
 
-    df.to_excel(filename, index=False)
+    df.to_excel(employee_file, index=False)
 
 
-def remove_employee(filename):
+def remove_employee(employee_file):
 
-    df=pd.read_excel(filename)
+    df=pd.read_excel(employee_file)
 
     id=int(input("Employee you want to remove (ID): "))
     index_to_remove=df[df['employee_id']==id].index
@@ -127,14 +152,14 @@ def remove_employee(filename):
     if confirm_remove=='Y':
         df=df.drop(index_to_remove)
         print(f"Employee {id} has successfully been removed\n")
-        df.to_excel(filename, index=False)
+        df.to_excel(employee_file, index=False)
     else:
         print("The action has been canceled\n")
 
 
-def update_employee(filename):
+def update_employee(employee_file):
 
-    df=pd.read_excel(filename)
+    df=pd.read_excel(employee_file)
 
     id_row=int(input("Which employee's info you want to update(ID): "))
     index_row_update = df[df['employee_id']==id_row]
@@ -165,28 +190,32 @@ def update_employee(filename):
     df.loc[row_index,column_name]=info
 
     print(f"Info {column_name} for Employee {id_row} has updated to {info}\n\n")
-    df.to_excel(filename, index=False)
+    df.to_excel(employee_file, index=False)
 
 
 def main():
-    filename="employee_data.xlsx"
-    while True:
-        print("Employee Payroll Management System")
-        print("A.Load File\nB.Adding Employee\nC.Remove Employee\nD.Update Employee's Information\nE.Generate Pay Slip\n"
-              "To Exit, Type X")
-        choice=input("What action you want to do: ")
-        if choice=='A':
-            load_data(filename)
-        elif choice=='B':
-            add_employee(filename)
-        elif choice=='C':
-            remove_employee(filename)
-        elif choice=='D':
-            update_employee(filename)
-        elif choice=='E':
-            generate_payslip(filename)
-        elif choice=='X':
-            break
+    employee_file="employee_data.xlsx"
+    salary_file="payslip_data.xlsx"
+    try:
+        while True:
+            print("Employee Payroll Management System")
+            print("A.Load File\nB.Adding Employee\nC.Remove Employee\nD.Update Employee's Information\nE.Generate Pay Slip\n"
+                  "To Exit, Type X")
+            choice=input("What action you want to do: ")
+            if choice=='A':
+                load_data(employee_file)
+            elif choice=='B':
+                add_employee(employee_file)
+            elif choice=='C':
+                remove_employee(employee_file)
+            elif choice=='D':
+                update_employee(employee_file)
+            elif choice=='E':
+                generate_payslip(employee_file,salary_file)
+            elif choice=='X':
+                break
+    except KeyboardInterrupt as err:
+        print("\nExit Successfully")
 
 
 if __name__=="__main__":
